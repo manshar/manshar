afeba480d9a308022471f6ca6e458e11f66cec38
Mohammad Khatib <mkhatib727@gmail.com>
2013-12-10 19:08:36 -0800

    Setup continious deployment of the master branch through Travis CI.

---
+++ utils/travis/deploy.sh
@@ -0,0 +1,24 @@
+if [[ "$TRAVIS_BRANCH" == "" ]]
+then
+  echo "TRAVIS_BRANCH variable is empty. This util is only to be used by Travis CI."
+  exit 1
+elif [[ "$TRAVIS_BRANCH" != "master" ]]
+then
+  echo "Not master. No Deploy to do."
+  exit 0
+else
+  echo "On master. Deploying now..."
+  wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+  git remote add web-client-heroku git@heroku.com:manshar-web-client.git
+  git remote add backend-heroku git@heroku.com:manshar-backend.git
+  echo "Host heroku.com" >> ~/.ssh/config
+  echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+  echo "   CheckHostIP no" >> ~/.ssh/config
+  echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+  heroku keys:clear
+  yes | heroku keys:add
+  yes | git push web-client-heroku `git subtree split --prefix web-client/dist master`:master --force
+  yes | git push backend-heroku `git subtree split --prefix backend master`:master --force
+  yes | heroku run rake db:migrate --app manshar-backend
+fi
+
