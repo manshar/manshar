90535fadcb58c9dea247296b2c2b432e5d98430e
Mohammad Khatib <mkhatib727@gmail.com>
2013-12-18 19:52:57 -0800

    Removed .DS_Store files and added heroku configs for the backend back.

--- backend/config/initializers/secret_token.rb
+++ backend/config/initializers/secret_token.rb
@@ -1,19 +1,4 @@
-# Be sure to restart your server when you modify this file.
+# This is not needed for an api-only application, but rails4
+# requires it to be defined, otherwise error is raised.
+Backend::Application.config.secret_key_base = ENV['SECRET_TOKEN']
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-
-# Although this is not needed for an api-only application, rails4 
-# requires secret_key_base or secret_toke to be defined, otherwise an 
-# error is raised.
-# Using secret_token for rails3 compatibility. Change to secret_key_base
-# to avoid deprecation warning.
-# Can be safely removed in a rails3 api-only application.
-Backend::Application.config.secret_token = 'cb4b8a63549e9ac14efe2c926758f6fbb92afb663666f728a77cb608aab3a33e75fab6e90b0dcb3ee620d2c7318e1d2317e25a9b96a86f33ba4dd30b20a48452'
