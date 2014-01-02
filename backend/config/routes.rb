890f87751c248aec91a34e45e13fcc1c961926b3
Mohammad Khatib <mkhatib727@gmail.com>
2014-01-01 21:32:44 -0800

    Redirecting the user to the web-client after confirming account.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,12 +3,14 @@ Backend::Application.routes.draw do
   devise_for :users, :controllers => {
     :sessions => 'sessions',
     :registrations => 'registrations',
+    :confirmations => 'confirmations',
   }
 
   namespace :api do
     namespace :v1 do
       devise_scope :user do
-        match 'registrations' => 'registrations#create', :as => 'register', :via => [:post, :options]
+        match 'confirm/:confirmation_token', :to => 'confirmations#show', :as => 'user_confirm', :via => [:get]
+        match 'registrations', :to => 'registrations#create', :as => 'register', :via => [:post, :options]
         match 'sessions', :to => 'sessions#create', :as => 'login', :via => [:post, :options]
         match 'sessions', :to => 'sessions#destroy', :as => 'logout', :via => [:delete, :options]
       end
