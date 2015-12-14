95eedb5ec4170cf81188baf240f2319581be64f9
Rasha Khatib <rasharasem@gmail.com>
2015-12-13 23:33:20 -0800

    First version of user profile

--- web-client/app/scripts/main.js
+++ web-client/app/scripts/main.js
@@ -1,32 +1,34 @@
+'use strict';
+
 $(document).ready(function(){
   $winWidth = $(window).width();
 
-  var menuStatus = "closed";
+  var menuStatus = 'closed';
 
-  $(".menu-icon, .menu").click(function(){
-    if(menuStatus === "closed") {
-      $(".body-wrapper, header").css("left","150px");
-      menuStatus = "opened";
+  $('.menu-icon, .menu').click(function(){
+    if(menuStatus === 'closed') {
+      $('.body-wrapper, header').css('left','150px');
+      menuStatus = 'opened';
     } else {
-      $(".body-wrapper, header").css("left","0px");
-      menuStatus = "closed";
+      $('.body-wrapper, header').css('left','0px');
+      menuStatus = 'closed';
     }
   });
 
   // show comments
-  $(".comments").click(function(){
+  $('.comments').click(function(){
     $el = $(this);
-    if($el.hasClass("collapsed")){
-      $(".single-article").css("color","#ccc");
-      $(".comments").hide();
+    if($el.hasClass('collapsed')){
+      $('.single-article').css('color','#ccc');
+      $('.comments').hide();
       $el.show();
-      $el.parent().css("color","#000");
-      $(".comment-content").fadeOut();
-      $el.find(".comment-content").fadeIn();
-      $el.find(".comment-image").css("opacity",1);
-      $el.removeClass("collapsed").addClass("expanded");
+      $el.parent().css('color','#000');
+      $('.comment-content').fadeOut();
+      $el.find('.comment-content').fadeIn();
+      $el.find('.comment-image').css('opacity',1);
+      $el.removeClass('collapsed').addClass('expanded');
       if($winWidth < 767) {
-        $(".content-wrapper").css("margin-right","150px");
+        $('.content-wrapper').css('margin-right','150px');
       }
     }
   });
@@ -35,16 +37,16 @@ $(document).ready(function(){
     offset = $(window).scrollTop();
 
     if(offset > 66) {
-    	$("header").css("top", "0");
-      if($(".content-wrapper .listing-types-tabs").length > 0) {
-        $("header .listing-types-tabs").html($(".content-wrapper .listing-types-tabs").html());
-      } 
+    	$('header').css('top', '0');
+      if($('.content-wrapper .listing-types-tabs').length > 0) {
+        $('header .listing-types-tabs').html($('.content-wrapper .listing-types-tabs').html());
+      }
     } else {
-    	$("header").css("top", "-48px");
+    	$('header').css('top', '-48px');
     }
   });
 
-  $(".to-top").click(function(){
-    $("body").stop().animate({scrollTop:0}, '500');
+  $('.to-top').click(function(){
+    $('body').stop().animate({scrollTop:0}, '500');
   });
 });
