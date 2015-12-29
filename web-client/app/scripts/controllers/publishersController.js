34df82e0a796c4bc8cef4b1d39edd8b2f95d4d22
Mohammad Khatib <mkhatib727@gmail.com>
2015-12-29 10:30:33 -0800

    Fix toolbars styling and other polishes.

--- web-client/app/scripts/controllers/publishersController.js
+++ web-client/app/scripts/controllers/publishersController.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('webClientApp')
-  .controller('PublishersCtrl', ['$scope', '$state', 'publishers',
-    function ($scope, $state, publishers) {
+  .controller('PublishersCtrl', ['$scope', '$state', '$anchorScroll', 'publishers',
+    function ($scope, $state, $anchorScroll, publishers) {
+      $anchorScroll();
       $scope.publishers = publishers;
       var currentState = $state.current.url;
       $scope.currentState = currentState.substring(0, currentState.length - 1);
