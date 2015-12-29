7796c532c92a69d13baedd7e95f6e8e58498c950
Rasha Khatib <rasharasem@gmail.com>
2015-12-28 22:37:04 -0800

    Replace hamburger menu with profile picture to indicate they're logged in

--- web-client/app/scripts/controllers/profileController.js
+++ web-client/app/scripts/controllers/profileController.js
@@ -10,7 +10,6 @@ angular.module('webClientApp')
 
     var currentState = $state.current.url;
     $scope.currentState = currentState.substring(0, currentState.length - 1);
-    console.log($scope.currentState);
 
     if (publishers) {
       $rootScope.$emit('publishers:loaded', {
