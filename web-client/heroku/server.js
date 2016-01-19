8001be8eed46eb8bc0ee89db691dcd6f4b328db8
Mohammad Khatib <mkhatib727@gmail.com>
2016-01-18 21:15:04 -0800

    Update list of crawlers agents for prerender.io

--- web-client/heroku/server.js
+++ web-client/heroku/server.js
@@ -1,13 +1,47 @@
 'use strict';
 
+var _ = require('lodash');
 var express = require('express');
-
 var app = module.exports = express();
 
 app.configure(function(){
-  // Here we require the prerender middleware that will handle requests from Search Engine crawlers
+  // Here we require the prerender middleware that will handle requests from
+  // Search Engine crawlers.
   // We set the token only if we're using the Prerender.io service
-  app.use(require('prerender-node').set('prerenderToken', 'zJohnQZtTAHTRGaDACW5'));
+  var prerender = require('prerender-node').set(
+      'prerenderToken', process.env.PRERENDER_TOKEN);
+
+  // These search engine bot do not adheres to google's _escaped_fragment_
+  // proposal, so we use user agent to detect them.
+  var moreCrawlerUserAgents = [
+      'Slurp!',
+      'MSNBot',
+      'YoudaoBot',
+      'JikeSpider',
+      'Sosospider',
+      '360Spider',
+      'Sogou web spider',
+      'Sogou inst spider',
+      'baiduspider',
+      'facebookexternalhit',
+      'twitterbot',
+      'rogerbot',
+      'linkedinbot',
+      'embedly',
+      'quora link preview',
+      'showyoubot',
+      'outbrain',
+      'pinterest',
+      'developers.google.com/+/web/snippet',
+      'slackbot',
+      'vkShare',
+      'W3C_Validator',
+      'redditbot'
+  ];
+  prerender.set('crawlerUserAgents', _.union(
+      prerender.crawlerUserAgents, moreCrawlerUserAgents));
+
+  app.use(prerender);
   app.use(express.compress());
 
   var options = {
